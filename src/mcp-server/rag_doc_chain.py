84155c1d1719efc0e74be61be6eda5853e0d9da8
Uriel Buitrago <uriel.buitrago@utexas.edu>
2025-07-20 22:18:42 -0500

    Basic MCP Server

--- src/mcp-server/rag_doc_chain.py
+++ src/mcp-server/rag_doc_chain.py
@@ -4,8 +4,8 @@ from typing_extensions import TypedDict, List
 
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
@@ -14,14 +14,14 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 dotenv.load_dotenv(PROJECT_ROOT + "/.env")
 CHROMA_DIR = os.environ.get("CHROMA_PERSIST_DIR", "chroma_db")
 RAG_TEMPLATE = """
-You are a meticulous Docker expert. Answer the question with the best of your knowledge, using the provided context as a reference.   
-
+You are a meticulous Docker expert, use the provided context for reference.   
+Your response should be concise, informative, and directly address the question.
+You will be replying to other LLM agents.
 <context>
 {context}
 </context>
 
 Question: {question}
-Answer with an explanation appropriate for a DevOps engineer:
 """
 prompt = PromptTemplate.from_template(RAG_TEMPLATE.rstrip())
 
@@ -57,7 +57,7 @@ else:
         collection_name="docker_docs",
     )
 
-llm = ChatOpenAI(model="gpt-4", temperature=0.5)
+llm = ChatOpenAI(model="gpt-4", temperature=0.2)
 
 def answer_question(question: str) -> str:
     # 1) retrieve
---
+++ src/mcp-server/server.py
@@ -0,0 +1,21 @@
+from mcp.server.fastmcp import FastMCP
+from rag_doc_chain import answer_question
+# Creat MCP server instance
+mcp = FastMCP("Docker Expert")
+
+@mcp.tool()
+def docker_docs(question: str) -> str:
+    """
+    RAG tool to answer questions about Docker using a knowledge base of Docker documentation.
+
+    Args:
+        question (str): The question to answer.
+
+    Returns:
+        str: The answer to the question.
+    """
+    return answer_question(question)
+
+if __name__ == "__main__":
+    # Launch the MCP server with the FastMCP interface
+    mcp.run(transport="stdio", port=3001)
